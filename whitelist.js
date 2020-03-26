1e397e835c5d8a659bc746f606badaead92d8af5
Checkium Folf <checkium@gmail.com>
2020-03-26 15:21:01 +0000

    Updated whitelist (thanks spotify-adblock-linux)

--- whitelist.js
+++ whitelist.js
@@ -1,27 +1,43 @@
 module.exports = [
-  "ap.spotify.com", // audio (access point)
-  "*.ap.spotify.com", // resolved access points
-  "apresolve.spotify.com", // access point resolving
-  "audio-sp-*.pscdn.co", // audio
-  "audio4-fa.scdn.co", // audio
-  "charts-images.scdn.co", // charts images
-  "dailymix-images.scdn.co", // daily mix images
-  "dealer.spotify.com", // discord rich presence
-  "i.scdn.co", // cover art
-  "lineup-images.scdn.co", // playlists lineup images
-  "merch-img.scdn.co", // merch images
-  "mosaic.scdn.co", // playlist mosaic images
-  "newjams-images.scdn.co", // release radar images
-  "o.scdn.co", // cover art
-  "pl.scdn.co", // playlist images
-  "profile-images.scdn.co", // artist profile images
-  "seeded-session-images.scdn.co", // radio images
-  "spclient.wg.spotify.com", // ads/tracking (blocked in blacklist), radio, recently played, friend activity,...
-  "t.scdn.co", // background images
-  "thisis-images.scdn.co", // 'this is' playlists images
-  "video-fa.scdn.co", // videos
-  "platform-lookaside.fbsbx.com", // facebook profile pics
-  "scontent.xx.fbcdn.net", // facebook profile pics
-  "login5.spotify.com", //checking login before playing music
-  "login4.spotify.com", //checking login before playing music
+    "localhost", // for using local proxies 
+    "audio-sp-*.pscdn.co", // audio
+    "audio-fa.scdn.co", // audio
+    "audio4-fa.scdn.co", // audio
+    "charts-images.scdn.co", // charts images
+    "daily-mix.scdn.co", // daily mix images
+    "dailymix-images.scdn.co", // daily mix images
+    "heads-fa.scdn.co", // audio (heads)
+    "i.scdn.co", // cover art
+    "lineup-images.scdn.co", // playlists lineup images
+    "merch-img.scdn.co", // merch images
+    "mosaic.scdn.co", // playlist mosaic images
+    "newjams-images.scdn.co", // release radar images
+    "o.scdn.co", // cover art
+    "pl.scdn.co", // playlist images
+    "profile-images.scdn.co", // artist profile images
+    "seeded-session-images.scdn.co", // radio images
+    "t.scdn.co", // background images
+    "thisis-images.scdn.co", // 'this is' playlists images
+    "video-fa.scdn.co", // videos
+    "content.production.cdn.art19.com", // podcasts
+    "rss.art19.com", // podcasts
+    "hwcdn.libsyn.com", // podcasts
+    "traffic.libsyn.com", // podcasts
+    "www.podtrac.com", // podcasts
+    "audio.simplecast.com", // podcasts
+    "media.simplecast.com", // podcasts
+    "ap.spotify.com", // audio (access point)
+    "*.ap.spotify.com", // resolved access points
+    "apresolve.spotify.com", // access point resolving
+    "dealer.spotify.com", // discord rich presence
+    "image-upload.spotify.com", // image uploading
+    "login*.spotify.com", // login
+    "spclient.wg.spotify.com", // ads/tracking (blocked in blacklist), radio, recently played, friend activity,...
+    "dcs*.megaphone.fm", // podcasts
+    "traffic.megaphone.fm", // podcasts
+    "heads4-ak-spotify-com.akamaized.net", // audio (heads)
+    "audio4-ak.spotify.com.edgesuite.net", // audio
+    "audio-sp-*.spotifycdn.net", // audio
+	"platform-lookaside.fbsbx.com", // facebook profile pics
+    "scontent.xx.fbcdn.net", // facebook profile pics
 ];
