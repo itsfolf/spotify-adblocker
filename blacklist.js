aea2c5f201683b7b21674676a58413468711882f
Folf <checkium@gmail.com>
2022-03-13 00:41:43 +0000

    Update blacklist.js

--- blacklist.js
+++ blacklist.js
@@ -1,7 +1,5 @@
 module.exports = [
-  "https://spclient.wg.spotify.com/ads/*", // ads
-  "https://spclient.wg.spotify.com/ad-logic/*", // ads
-  "https://spclient.wg.spotify.com/gabo-receiver-service/*", // tracking
-  "https://gew1-spclient.spotify.com/gabo-receiver-service/*", // tracking
-  "https://gew1-spclient.spotify.com/quicksilver/*", // tracking
+  "https://spclient.wg.spotify.com/ads/.*", // ads
+  "https://spclient.wg.spotify.com/ad-logic/.*", // ads
+  "https://spclient.wg.spotify.com/gabo-receiver-service/.*", // tracking
 ];
